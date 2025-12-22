af4dd6c399487fcd889b5c2861502c06b4531df9
Anas <86509653+AnasDharar@users.noreply.github.com>
2025-12-22 21:53:43 +0530

    initial layout of landing page

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import LandingPage from './components/LandingPage.jsx'
+function App() {
+
+  return (
+    <>
+      <LandingPage></LandingPage>
+    </>
+  )
+}
+
+export default App
